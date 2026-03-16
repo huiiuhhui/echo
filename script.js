2b7895e6efd1d41c76bdc48de81015ea93cdc91a
huihui <h6084220@gmail.com>
2026-03-16 19:04:58 +0800

    vue框架迁移

--- script.js
+++ script.js
@@ -64,34 +64,11 @@ btnThemeToggle.addEventListener('click', toggleTheme);
 
 ///////////////////////////////////////
 // Button scrolling
-btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-
-  // Scrolling
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
-
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+if (btnScrollTo) {
+  btnScrollTo.addEventListener('click', function (e) {
+    section1.scrollIntoView({ behavior: 'smooth' });
+  });
+}
 
 ///////////////////////////////////////
 // Page navigation
